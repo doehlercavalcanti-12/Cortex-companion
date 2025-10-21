98af1044dfed64e613fed86cba1f8b64f929390c
doehlercavalcanti-12 <doehlercavalcanti@gmail.com>
2025-10-21 18:04:23 -0300

    fix: forward user controller errors

--- backend/src/application/interfaces/controllers/UserController.ts
+++ backend/src/application/interfaces/controllers/UserController.ts
@@ -1,13 +1,21 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { CreateUserUseCase } from '../../use_cases/CreateUserUseCase';
 import { validator } from '../validators/userValidator';
 
 export class UserController {
   constructor(private readonly createUser: CreateUserUseCase) {}
 
-  register = async (request: Request, response: Response): Promise<Response> => {
-    const payload = validator.parse(request.body);
-    const user = await this.createUser.execute(payload);
-    return response.status(201).json({ id: user.id, email: user.email });
+  register = async (
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ): Promise<Response | void> => {
+    try {
+      const payload = validator.parse(request.body);
+      const user = await this.createUser.execute(payload);
+      return response.status(201).json({ id: user.id, email: user.email });
+    } catch (error) {
+      return next(error);
+    }
   };
 }
